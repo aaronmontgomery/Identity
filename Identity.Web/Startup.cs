d06d55d31ac0b37f148631e6d6dd00c1c574a3f5
aaronmontgomery <aaronmmontgomery@students.abtech.edu>
2020-04-24 04:08:31 -0400

    removed UseHealthChecks

--- Identity.Web/Startup.cs
+++ Identity.Web/Startup.cs
@@ -35,7 +35,7 @@ namespace Identity.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseHealthChecks("/health");
+            //app.UseHealthChecks("/health");
             app.UseExceptionHandler("/error");
             app.UseHttpsRedirection();
             app.UseHsts();
